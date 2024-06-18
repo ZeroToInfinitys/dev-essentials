a4b4688ef504fb01c167ac0ba59317e2adb10687
徐建明 <1469630570@qq.com>
2024-06-18 15:11:30 +0800

    feat: 第一次提交vue3学习代码

---
+++ vue3-project/src/components/Person.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="person">
+    <h2>姓名: {{ name }}</h2>
+    <h2>年龄: {{ age }}</h2>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Person',
+  setup () {
+    
+
+    return {}
+  }
+}
+</script>
+ 
+<style lang="scss" scoped>
+.person {
+  background-color: #ddd;
+  width: 100px;
+  height: 100px;
+  border-radius: 50%
+}
+</style>
